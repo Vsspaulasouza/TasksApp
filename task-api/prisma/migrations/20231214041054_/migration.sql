c939b6a047cc32830b8a7a7c63fcb8fb7d135872
Vinícius Soares <vsspaulasouza@gmail.com>
2023-12-14 01:11:09 -0300

    changing schema

--- task-api/prisma/migrations/20231214041054_/migration.sql
+++ task-api/prisma/migrations/20231214041054_/migration.sql
@@ -1,11 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('TODO', 'DOING', 'DONE');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "username" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
-    "salt" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -15,26 +19,10 @@ CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
     "authorId" INTEGER NOT NULL,
-    "priorityId" INTEGER,
-    "statusId" INTEGER,
+    "priority" "Priority" NOT NULL,
+    "status" "Status" NOT NULL,
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Priority" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-
-    CONSTRAINT "Priority_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Status" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-
-    CONSTRAINT "Status_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -63,12 +51,6 @@ CREATE INDEX "_CategoryToTask_B_index" ON "_CategoryToTask"("B");
 
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_priorityId_fkey" FOREIGN KEY ("priorityId") REFERENCES "Priority"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Task" ADD CONSTRAINT "Task_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "_CategoryToTask" ADD CONSTRAINT "_CategoryToTask_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
