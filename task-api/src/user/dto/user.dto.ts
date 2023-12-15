f499f84b1a94b4c64cd5eb3972ca4fc8eb4f9430
Vinícius Soares <vsspaulasouza@gmail.com>
2023-12-15 00:31:17 -0300

    fix: update user dto (empty name)

--- task-api/src/user/dto/user.dto.ts
+++ task-api/src/user/dto/user.dto.ts
@@ -1,7 +1,8 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
+  @IsNotEmpty()
   name?: string;
 }
