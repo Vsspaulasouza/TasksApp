f8c4b553ea735a28df7072232d67df671659663f
Vinícius Soares de Paula Souza <vsspaulasouza@gmail.com>
2024-02-02 16:39:17 -0300

    chore: add filter tasks to options bar

--- task-frontend/src/components/optionsBar/OptionsBar.tsx
+++ task-frontend/src/components/optionsBar/OptionsBar.tsx
@@ -1,5 +1,6 @@
 import { HStack } from "@chakra-ui/react";
 import { CreateTask } from "./CreateTask";
+import { FilterTasks } from "./FilterTasks";
 import { ManageCategories } from "./ManageCategories";
 
 export function OptionsBar() {
@@ -7,6 +8,7 @@ export function OptionsBar() {
     <HStack pt={{ base: "8", md: "10" }} pb={{ base: "3", md: "5" }}>
       <CreateTask />
       <ManageCategories />
+      <FilterTasks />
     </HStack>
   );
 }
